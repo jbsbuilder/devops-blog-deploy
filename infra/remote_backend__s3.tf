a7ad96e9e6150dbc94c20deaff7961c542ba181f
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-19 19:10:39 -0600

    updated remote s3 bucket

--- infra/remote_backend__s3.tf
+++ infra/remote_backend__s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-1"
+    bucket = "devops-blog"
     key    = "devops-project-1/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-1""
   }
 }
