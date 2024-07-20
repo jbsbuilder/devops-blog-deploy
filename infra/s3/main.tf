4251ce0c71d4056b77ee9a5fe95870c8deeb3c62
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-19 22:40:05 -0600

    hi

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,11 +1,11 @@
 variable "bucket_name" {}
 variable "name" {}
 variable "environment" {}
-
+/*
 output "remote_state_s3_bucket_name" {
   value = aws_s3_bucket.remote_state_bucket.id
 }
-/*
+
 resource "aws_s3_bucket" "remote_state_bucket" {
   bucket = var.bucket_name
 
