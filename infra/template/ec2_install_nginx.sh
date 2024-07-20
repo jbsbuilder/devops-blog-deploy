1f974775e60710de59872428e6c72910e35f8b89
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-19 23:37:10 -0600

    hi

--- infra/template/ec2_install_nginx.sh
+++ infra/template/ec2_install_nginx.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install -y nginx nodejs npm git
+apt-get update -y
+apt-get install -y nginx nodejs npm git
 
-sudo ufw allow 80
-sudo ufw allow 443
+ufw allow 80
+ufw allow 443
 
-sudo git clone https://github.com/jbsbuilder/project-blog.git
+git clone https://github.com/jbsbuilder/project-blog.git
 
 # Update directory
-sudo cd project-blog
+cd project-blog
 
-sudo npm install
-sudo npm run build
+npm install
+npm run build
 
 # Configure Nginx to serve the Svelte app Update svelte path
-sudo cat <<EOF > /etc/nginx/sites-available/project-blog
+cat <<EOF > /etc/nginx/sites-available/project-blog
 server {
     listen 80;
     server_name _;
@@ -28,12 +28,12 @@ server {
 }
 EOF
 
-sudo ln -s /etc/nginx/sites-available/project-blog /etc/nginx/sites-enabled/project-blog
+ln -s /etc/nginx/sites-available/project-blog /etc/nginx/sites-enabled/project-blog
 
-sudo rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-enabled/default
 
-sudo nginx -t
+nginx -t
 
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+systemctl restart nginx
+systemctl enable nginx
 
