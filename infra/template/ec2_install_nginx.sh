45e53e2b3b5f657f80d073a2aeec44f5b1f649b1
jbsbuilder <work.jacobsmith@gmail.com>
2024-07-22 00:22:15 -0600

    template script updated

--- infra/template/ec2_install_nginx.sh
+++ infra/template/ec2_install_nginx.sh
@@ -1,47 +1,27 @@
 #!/bin/bash
 
-# Update package lists
-sudo yum update -y
+sudo dnf update -y
 
-# Install nginx, nodejs, npm, and git
-sudo yum install -y nodejs npm git nginx
+sudo dnf install httpd git nodejs -y
 
-# Allow HTTP and HTTPS traffic through the firewall using iptables
-sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
-sudo service iptables save
+sudo systemctl start httpd
 
-# Start and enable Nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl enable httpd
 
-# Clone the project repository
+sudo usermod -a -G apache ec2-user
+
+exit
+
+#these commands must be ran after set up
+
+<sudo chown -R ec2-user:apache /var/www
+
 git clone https://github.com/jbsbuilder/project-blog.git
 
-# Update directory to project-blog
 cd project-blog
 
-# Install npm dependencies and build the project
 npm install
+
 npm run build
 
-# Configure Nginx to serve the Svelte app
-sudo bash -c 'cat <<EOF > /etc/nginx/conf.d/project-blog.conf
-server {
-    listen 80;
-    server_name _;
-
-    root /home/ec2-user/project-blog/build; # Update this path to the correct build output
-    index index.html;
-
-    location / {
-        try_files \$uri /index.html;
-    }
-}
-EOF'
-
-# Test the Nginx configuration for syntax errors
-sudo nginx -t
-
-# Restart Nginx to apply the new configuration
-sudo systemctl restart nginx
+sudo cp -r dist/* /var/www/html/>
